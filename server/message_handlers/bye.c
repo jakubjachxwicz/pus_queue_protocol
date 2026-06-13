3d8694ed4a80f9e635954b095514f31586ea0bcd
Jakub Jachowicz <KubaJachowicz39@gmail.com>
2026-06-13 16:02:55 +0200

    [server] ughg

--- server/message_handlers/bye.c
+++ server/message_handlers/bye.c
@@ -1,3 +1,22 @@
-//
-// Created by KubaJ on 13.06.2026.
-//
+#include "handlers.h"
+
+
+void handle_bye(client_conn_t *conn, const char *msg)
+{
+    char ts[32];
+    iso_timestamp(ts, sizeof(ts));
+
+    char bye[256];
+    snprintf(bye, sizeof(bye),
+        "{\"type\":\"BYE\","
+        "\"message_id\":\"%s\","
+        "\"timestamp\":\"%s\","
+        "\"session_id\":\"%s\"}",
+        conn->session_id,
+        ts,
+        conn->session_id);
+
+    ssl_send(conn->ssl, bye);
+
+    printf("[%s] BYE received, closing session %s\n", conn->ip, conn->session_id);
+}
